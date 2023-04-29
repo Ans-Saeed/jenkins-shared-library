1f86a50b9c18a406d0e5b324d97a282beaa69284
Ans <anssaeed.pk@gmail.com>
2023-04-29 19:35:23 +0500

    Fixed SDK issue and some errors

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 package com.example
-
+import java.io.Serializable
 class Docker implements Serializable {
  def script
     Docker(script){
         this.script=script
     }
-    def builddockerimage(String image){
+    def builddockerimage(String name){
         script.echo 'Building the image ... '
         script.sh "docker build -t $script.name ."
     }
@@ -16,7 +16,7 @@ class Docker implements Serializable {
                     script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
                 }
     }
-    def dockerPush(String image){
+    def dockerPush(String name){
         script.sh "docker push $script.name"
     }
 }
